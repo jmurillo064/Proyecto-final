4896e17170658b732d6d89c9825ed9d2fee7e36b
jmurillo064 <jmurillo064@gmail.com>
2022-09-26 21:46:45 -0500

    Servidor listo

--- src/app/servicios/roya.service.ts
+++ src/app/servicios/roya.service.ts
@@ -10,20 +10,9 @@ export class RoyaService {
 
   predecirRoya(data: any){
     let  url = 'http://approya.pythonanywhere.com/predecir';
-   // let  url = 'http://127.0.0.1:5000/predecir';
-    var formData = new FormData(); 
-    formData.append('base64img',data.base64img);
-    formData.append('Altura_planta',data.Altura_planta);
-    formData.append('Numero_ramas',data.Numero_ramas);
-    formData.append('Nivel_produccion',data.Nivel_produccion);
-    formData.append('Plagas',data.Plagas);
-    formData.append('Nivel_plagas',data.Nivel_plagas);
-    formData.append('Nivel_otras_enfermedades',data.Nivel_otras_enfermedades);
-    formData.append('Produccion_gramos',data.Produccion_gramos);
-    //let params = new HttpParams();
-    //params.append('base64img',data.base64img);
+    //let  url = 'http://jmurillo064.pythonanywhere.com/predecir';
     return new Promise((resolve, reject) => {
-      //this.http.post(url,JSON.stringify(data)).subscribe(res => { 
+      //this.http.get(url).subscribe(res => { 
       this.http.post(url,JSON.stringify(data)).subscribe(res => { 
       resolve(res);
         }, error => { 
